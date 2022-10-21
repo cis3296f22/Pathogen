0824a2fb4d4d1b53443740d92fe0edfeef812cd2
Ryan Hodge <ryanhodge240@gmail.com>
2022-10-20 20:04:02 -0400

    Setting up the react framework with simple p5.js

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Visual from './components/Visual';
+
+function App() {
+	return (
+		<div className="App">
+			<Visual></Visual>
+		</div>
+	);
+}
+
+export default App;
