e571b5177b830ef4b761a2e7710d5515b2998102
Ryan Hodge <ryanhodge240@gmail.com>
2022-11-14 22:28:03 -0500

    React rendering components is now working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import renderer from 'react-test-renderer';
+import Banner from './components/Banner';
+import Constants from './tools/Constants';
+
+beforeAll(() => 
+Object.defineProperty(window, 'matchMedia', {
+	writable: true,
+	value: jest.fn().mockImplementation(query => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // Deprecated
+		removeListener: jest.fn(), // Deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
+	})),
+}));
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	let element = renderer.create(<Banner setParameters={() => {}} params={Constants.PARAMS}/>);
+	expect(element.root.props.params).toEqual(Constants.PARAMS);
 });
