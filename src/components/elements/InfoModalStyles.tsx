5c5fcc566a466bd1597759f638d8754ad94cadff
Ryan Hodge <ryanhodge240@gmail.com>
2022-11-26 14:52:17 -0500

    Fixing some of the tabs

--- src/components/elements/InfoModalStyles.tsx
+++ src/components/elements/InfoModalStyles.tsx
@@ -9,8 +9,8 @@ export default class InfoModalStyles {
     static readonly InfoButton = styled(IconButton)`
         font-size: calc(var(--vh) * .02);
         position: absolute;
-        top 0;
-        left 0;
+        top: 0;
+        left: 0;
         margin: 10px;
     `
 
@@ -18,35 +18,20 @@ export default class InfoModalStyles {
         .modal-dialog {
             width: 75vw;
             max-width: 100vw;
-
         }
 
-        .modal-content {background-color: unset;}
+        .modal-content { background-color: unset; }
     
         .modal-header, .modal-body, .modal-footer {
             background: ${Colors.PRIMARY_TRANSPARENT};
             color: white;
             border-color: #FFFFFFAF;
         }
+
         .modal-footer { text-align: center; }
 
-        .react-tabs {
-            color: white;
-            background: ${Colors.PRIMARY_TRANSPARENT};
-            
-            .react-tabs__tab-list {
-                display: grid;
-                grid-template-columns: 50% 50%;
-                text-align: center;
-                border-bottom: none;
-                bottom: 0px;
-            }
-
-            .react-tabs__tab-panel {
-                h2 {text-align: center;}
-                padding: 20px;
-                white-space: pre-line;
-            }
+        .react-tabs__tab:focus:after {
+            background-color: unset;
         }
 	`
 
